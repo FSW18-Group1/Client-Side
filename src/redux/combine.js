0505b109c325315208a485e8d4e70d2b0a596a28
febriansyahfar <febriansyahfar.ff@gmail.com>
2022-06-28 21:44:35 +0700

    new commit

--- src/redux/combine.js
+++ src/redux/combine.js
@@ -1,13 +1,12 @@
 import { combineReducers } from "redux";
 import authenticatedReducer from "./reducers/authenticated";
-import leaderboardReducer from "./reducers/leaderboard"
+import leaderboardReducer from "./reducers/leaderboard";
 import ProfilePlayerReducer from "./reducers/profileplayer";
+import profileReducer from "./reducers/profile";
 
-export default combineReducers(
-    {
-        authenticatedReducer,
-        leaderboardReducer,
-        ProfilePlayerReducer
-        
-    }
-)
+export default combineReducers({
+  authenticatedReducer,
+  leaderboardReducer,
+  ProfilePlayerReducer,
+  profileReducer,
+});
